7e3f8598028a2d4a0e5ba6c3daa3a1873984b4cf
Sinchan Maitra <smsinchanmaitra2007@gmail.com>
2024-12-13 05:28:33 +0000

    a test file

--- textanalyser/src/app/(tools)/analyser/page.tsx
+++ textanalyser/src/app/(tools)/analyser/page.tsx
@@ -1,14 +1,19 @@
 import { Tools } from "@/utils/analyser";
 
+// TEST
 export default async function Home() {
-  const _string = "I am a23 bad boy";
-  const analyserEngine = new Tools.Analyser(_string, { removenum: true });
+  const _string = "I am a23 bad boy https://chatgpt.com/c/675bc0ee-1390-800e-8896-b758070540b1 https://www.typescriptlang.org/docs/handbook/2/classes.html";
+  const analyserEngine = new Tools.Analyser(_string, { removepunc: true, removenum: true, removealpha: true });
   const result = await analyserEngine.main();
   const purpose = result["purpose"];
   const out = result['output'];
   return (
     <div>
-      <code>{out}</code>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <code>{out}</code><br/>
       <code>{purpose}</code>
     </div>
   );
